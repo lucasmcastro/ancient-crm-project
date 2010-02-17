ca9f9d830a4e3964fdc677c390a29bdac0b5ea64
Lucas Castro <luke@jonatas-desktop.(none)>
2010-02-18 08:37:09 +0900

    Ticket: http://veezor.lighthouseapp.com/projects/35342/tickets/7-adicionar-soma-do-valor-das-oportunidades-existentes-de-determinada-conta

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,18 +1,84 @@
 class Account < ActiveRecord::Base
+  # Relacionamentos
   belongs_to :manager, :class_name => 'User'
   belongs_to :creator, :class_name => 'User'
 
   has_one :address, :as => :addressable
   has_many :opportunities
   
+  # Atributos acessíveis para 'mass-assignment'
   attr_accessible :name, :manager_id
   
+  # Atributos obrigatórios
   validates_presence_of :name
-  
   validates_presence_of :creator
   validates_presence_of :manager
   
+  # Verificar consistência de objetos relacionados
   validates_associated :creator
   validates_associated :manager
 
+  # Filters
+  named_filter(:interactions) do |account_id|
+    with(:id, account_id)
+    having(:opportunities) do
+      having(:interactions)
+    end
+  end  
+
+  named_filter(:issues) do |account_id|
+    with(:id, account_id)
+    having(:opportunities) do
+      having(:issues)
+    end
+  end
+
+  named_filter(:open_issues) do |account_id|
+    with(:id, account_id)
+    having(:opportunities) do
+      having(:issues) do
+        having(:status) do
+          any_of do
+            with(:name).in ['Novo', 'Atribuído', 'Reaberto'] 
+          end
+        end
+      end
+    end
+  end
+
+  # Definições
+  def interactions_sum
+    (Account.interactions self.id).count
+  end
+    
+  def issues_sum
+    (Account.issues self.id).count
+  end
+
+  def open_issues_sum
+    Account.open_issues(self.id).count
+  end
+
+# Adicionar noção de última interação, me fez pensar melhor na minha modelagem
+# e decidi que preciso pensar mais antes de adicionar essa noção.
+# É provável que a modelagem e abordagem do sistema quanto as interações mude
+# para que ele fique mais usável.
+# Por hora, o código fica aqui como histórico do que foi feito, porém não
+# será utilizado por enquanto.
+#  def last_interaction
+#    account_id = self.id
+#    Note.filter do
+#      with(:commentable_type, 'Interaction')
+#      join(Interaction, :join_type => :inner) do
+#        on(:commentable_id => :id)
+#        having(:opportunity) do
+#          having(:account) do
+#            with(:id, account_id)
+#          end
+#        end
+#      end
+#      order(:created_at, :desc)
+#      limit(1)
+#    end
+#  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,9 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  
+  # -- Plugins e Gems --
+  # Record Filter
+  #   Permite fazer queries de SQL através de uma DSL
+  config.gem 'aub-record_filter', :lib => 'record_filter', :source => 'http://gems.github.com'
 end
