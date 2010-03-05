1d6e35cf1c8a6a6f3ba2126dc650e21241372aee
Lucas Castro <luke@jonatas-desktop.(none)>
2010-03-05 18:21:11 -0300

    Ticket: http://veezor.lighthouseapp.com/projects/35342/tickets/26-gerenciamento-bsico-de-usurios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,64 @@
 class UsersController < ApplicationController  
   
+  # render index.rhtml
+  def index
+    @users = User.all
+  end
 
   # render new.rhtml
   def new
     @user = User.new
   end
  
+  # render edit.rhtml
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  # POST /users.xml
   def create
-    logout_keeping_session!
     @user = User.new(params[:user])
-    success = @user && @user.save
-    if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-    else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => 'new'
+    
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = 'Usuário foi criado com sucesso.'
+        format.html { redirect_to home_path }
+        format.xml  { render :xml => @user, :status => :created, :location => home_path }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  # PUT /users/1
+  # PUT /users/1.xml
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(users_path) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+    
+  # DELETE /users/1
+  # DELETE /users/1.xml
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy unless @user.login == 'admin'
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
     end
   end
 end
